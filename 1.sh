e248b411a22ee24f74931fc593cf29cab2809fa9
yml <yml>
2016-11-16 20:35:27 +0800

    jiaohu

---
+++ 1.sh
@@ -0,0 +1,6 @@
+#! bin/bash
+# this my first shell
+# write by yml
+ d=`date +%y%m%d`
+echo "this is my frist shell" 
+echo "write on $d. "
---
+++ jiaohu.sh
@@ -0,0 +1,4 @@
+#! bin/bash
+read -p "please input a number : " num
+echo "your number is $num"
+
