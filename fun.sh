550d4e227a922e75e126ae902144bf63ed499b28
yml <yml>
2016-11-17 05:52:05 +0800

    if

---
+++ fun.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+ function sum(){
+  a=$1
+   b=$2
+ sum=$[ $1 + $2 ]
+   echo $sum
+ }
+x=1
+y=2
+s=`sum x y`
+echo $s
---
+++ if.sh
@@ -0,0 +1,6 @@
+#!/bin/bah
+ read -p "please input your score:" a
+ if ((a<60));then 
+echo "you didn't pas the exam"
+else  echo "good you passed the exam." 
+fi
